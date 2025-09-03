df2e38aaaf78a04994e2a6a5f596324739239c93
ycna <758049824@qq.com>
2025-09-03 15:52:13 +0800

    error reslove

--- libwrt.sh
+++ libwrt.sh
@@ -19,27 +19,24 @@ rm -rf package/emortal/luci-app-athena-led
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
 
-# 在 OpenWrt 源码的 package/kernel/linux/modules/netsupport.mk 文件中添加以下内容：
-echo 'define KernelPackage/xdp-sockets-diag
-SUBMENU:=$(NETWORK_SUPPORT_MENU)
-TITLE:=PF_XDP sockets monitoring interface support for ss utility
-KCONFIG:= \
-CONFIG_XDP_SOCKETS=y \
-CONFIG_XDP_SOCKETS_DIAG
-FILES:=$(LINUX_DIR)/net/xdp/xsk_diag.ko
-AUTOLOAD:=$(call AutoLoad,31,xsk_diag)
-endef
-define KernelPackage/xdp-sockets-diag/description
-Support for PF_XDP sockets monitoring interface used by the ss tool
-endef
-$(eval $(call KernelPackage,xdp-sockets-diag))' >> package/kernel/linux/modules/netsupport.mk
+# # 在 OpenWrt 源码的 package/kernel/linux/modules/netsupport.mk 文件中添加以下内容：
+# echo 'define KernelPackage/xdp-sockets-diag
+# SUBMENU:=$(NETWORK_SUPPORT_MENU)
+# TITLE:=PF_XDP sockets monitoring interface support for ss utility
+# KCONFIG:= \
+# CONFIG_XDP_SOCKETS=y \
+# CONFIG_XDP_SOCKETS_DIAG
+# FILES:=$(LINUX_DIR)/net/xdp/xsk_diag.ko
+# AUTOLOAD:=$(call AutoLoad,31,xsk_diag)
+# endef
+# define KernelPackage/xdp-sockets-diag/description
+# Support for PF_XDP sockets monitoring interface used by the ss tool
+# endef
+# $(eval $(call KernelPackage,xdp-sockets-diag))' >> package/kernel/linux/modules/netsupport.mk
 
 # 在 .config 文件中启用该模块：
-echo 'CONFIG_PACKAGE_kmod-xdp-sockets-diag=y' >> .config
+# echo 'CONFIG_PACKAGE_kmod-xdp-sockets-diag=y' >> .config
 
-git clone --depth=1 https://github.com/QiuSimons/luci-app-daed package/luci-app-daed
-
-git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
 
 
